e152f1d310fd4f6bd1717275902e3033ba347a1e
Tom Choo <thchoo@snsoft.my>
2018-12-14 18:11:39 +0800

    html structure part 1

---
+++ src/components/ProductDetail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+        <p>Global Collection Kids Girls Chiffon Lace Princess </p>
+        <p>Wedding Bridesmaid Pageant Party Formal </p>
+        <p>Dresses</p>
+        <div>
+            <span> 5 stars </span><span> 22 Ratings </span><span> share </span><span> heart </span>
+        </div>
+        <div>
+            <span>Brand: </span><span> OEM | More Girls from OEM</span>
+        </div>        
+        
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductDetail",
+  props: [],
+  methods: {
+  }
+};
+</script>
+
+<style>
+</style>
