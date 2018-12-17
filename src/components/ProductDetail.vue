bd4552985265befb87fe47c5b91efc24376da626
Tom Choo <thchoo@snsoft.my>
2018-12-17 16:59:00 +0800

    brand and color family

--- src/components/ProductDetail.vue
+++ src/components/ProductDetail.vue
@@ -6,33 +6,36 @@
         <span class="spacer"></span>
         <i class="fas fa-share-alt"></i><i class="far fa-heart" style="marginLeft: 23px"></i>
     </div>
-    <div>
-        <span>Brand: </span><span> OEM | More Girls from OEM</span>
-    </div>        
-    <hr>
-    <p>RM53.00</p>
-    <p>RM88.40 -40%</p>
-    <hr>
+    <div class="brand">
+        <span class="text">Brand: </span><a>OEM</a><span>&nbsp;|&nbsp;</span><a>Girls from OEM</a>
+    </div>
+    <p class="price">RM53.00</p>
+    <p class="previous-price"><span style="textDecoration: line-through" class="text">RM88.40</span> -40%</p>
     <!-- color family -->
-    <div>
-      <div>Color Family</div>
-      <div>
-        <div>Blue</div>
-        <div>colors</div>
+    <div class="flex-container">
+      <div class="left-column">Color Family</div>
+      <div class="right-column">
+        <p>Blue</p>
+        <div class="color-list">
+          <span class="color-item" style="borderColor: #f57224"></span>
+          <span class="color-item"></span>
+          <span class="color-item"></span>
+          <span class="color-item"></span>
+        </div>
       </div>
     </div>
     <!-- size -->
-    <div>
-      <div>Size</div>
-      <div>
+    <div class="flex-container">
+      <div class="left-column">Size</div>
+      <div class="right-column">
         <div>Int</div>
         <div>years</div>
       </div>
     </div>
     <!-- qty -->
-    <div>
-      <div>Quantity</div>
-      <div>qty</div>
+    <div class="flex-container">
+      <div class="left-column">Quantity</div>
+      <div class="right-column">qty</div>
     </div>
     <div>
       <button>Buy Now</button>
@@ -60,6 +63,7 @@ export default {
   font-size: 22px;
   margin-top: 13px;
   margin-bottom: 11px;
+  font-weight: normal;
 }
 .rating{
   display: flex;
@@ -71,5 +75,56 @@ export default {
 }
 .fas, .far{
   color: #757575;
+  font-size: 18px;
+  cursor: pointer;
+}
+.fas:hover, .far:hover{
+  color: #f57224;
+}
+.brand{
+  line-height: 20px;
+  padding-bottom: 15px;
+  border-bottom: 1px solid #eff0f5;
+}
+.price{
+  color: #f57224;
+  font-size: 30px;
+  padding-top: 16px;
+}
+.previous-price, .previous-price span{
+  font-size: 14px;
+}
+.previous-price{
+  padding-bottom: 17px;
+  border-bottom: 1px solid #eff0f5;
+}
+.flex-container{
+  display: flex;
+  padding-top: 25px;
+}
+.left-column{
+  width: 92px;
+  color: #757575;
+  font-size: 14px;
+}
+.right-column{
+  font-size: 14px;
+}
+.color-list{
+  display: flex;
+  margin-top: 8px;
+}
+.color-item{
+  width: 32px;
+  height: 32px;
+  padding: 1px;
+  border: 1px solid #eff0f5;
+  margin-right: 12px;
+  cursor: pointer;
+  background-clip: content-box; /*define the extent of background*/
+  background-color: grey;
+}
+.color-item:hover{
+  border-color: #f57224;
 }
 </style>
