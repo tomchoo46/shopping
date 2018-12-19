3b02d7f458628324f2e0117a7655d192a6309731
Tom Choo <thchoo@snsoft.my>
2018-12-19 10:28:55 +0800

    delivery information html structure

--- src/components/DeliveryInfo.vue
+++ src/components/DeliveryInfo.vue
@@ -1,6 +1,25 @@
 <template>
-  <div class="">
-    <p>delivery-info</p>
+  <div>
+    <p>Delivery Information</p>
+    <div>
+      <div>
+        <p>Full name</p>
+        <input type="text">
+        <p>Phone Number</p>
+        <input type="text">
+      </div>
+      <div>
+        <p>Address</p>
+        <input type="text">
+        <p>State</p>
+        <input type="text">
+        <p>City</p>
+        <select name="" id=""></select>
+        <p>Postcode</p>
+        <select name="" id=""></select>
+        <button>Save</button>
+      </div>
+    </div>
   </div>
 </template>
 
