4ef15c95d87979b19759d6cda77ebdb143783e2e
Tom Choo <thchoo@snsoft.my>
2018-12-19 10:22:18 +0800

    order detail html structure

--- src/components/OrderDetail.vue
+++ src/components/OrderDetail.vue
@@ -1,6 +1,37 @@
 <template>
-  <div class="">
-    <p>order-detail</p>
+  <div>
+    <div>
+      <div>#5 ITEMS</div>
+      <div>PRICE</div>
+      <div>QUANTITY</div>
+    </div>
+    <div><span>Fulfilled by</span><span>#MM88 Store</span></div>
+    <div>
+      <p>Delivery Option</p>
+      <div>
+        <span>Icon</span>
+        <div>
+          <p><span>Icon</span><span>Economy</span></p>
+          <p>Get By 26 Dec-10 Jan 2019</p>
+          <p>RM7.05</p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <img src="" alt="product">
+          <div>
+            <p>#title</p>
+            <p><span>#brand</span><span>, Size:Int</span><span>#size</span><span>, Color Family:</span><span>#color</span></p>
+          </div>
+        </div>
+        <div>
+          <p>#price</p>
+          <p>#originalPrice</p>
+          <p>#discount</p>
+        </div>
+        <div><span>Qty: </span><span>#Qty</span></div>
+      </div>
+    </div>
   </div>
 </template>
 
